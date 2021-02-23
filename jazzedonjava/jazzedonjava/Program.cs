92387585771b714ee03ef3e496b9cd3cec6b4d25
jmckelvey786 <mckelveywarner@gmail.com>
2021-02-23 14:29:27 -0500

    intial commit

---
+++ jazzedonjava/jazzedonjava/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace jazzedonjava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jazzed on Java!");
+            bool hascoffee;
+            Console.WriteLine("You have had a cup of coffee? 'true' or 'false'");
+            hascoffee = bool.Parse(Console.ReadLine());
+            if (hascoffee)
+            {
+                Console.WriteLine("Jazzed!");
+            }
+            else
+            {
+                Console.WriteLine("Not jazzed");
+            }
+        }
+    }
+}
